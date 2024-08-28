86c00d985f2e420d339fcf4b43e26b6b0698d724
rcosouza_meli <marcos.msouza@mercadolivre.com>
2024-08-27 22:24:05 -0300

    Initial Commit

---
+++ RulerWheelPicker/RulerWheelPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RulerWheelPickerApp.swift
+//  RulerWheelPicker
+//
+//  Created by mfelipesp on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RulerWheelPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
